83647c996292d5b9a672a10bb75aa31bda1afc16
joaoparella <joao.pparella@sp.senac.br>
2024-07-02 19:43:02 -0300

    classe data.js

--- principal.js
+++ principal.js
@@ -1,25 +1,27 @@
 const Usuario = require('./Usuario.js')
 const entradaSaida = require('./EntradaSaida.js')
+const Datas = new (require('./datas.js'))()
 const EntradaSaida = new entradaSaida();
 
 const usuarioJoao = new Usuario("joao", "joao@teste.com", "123");
 
 const usuarioRoberto = new Usuario("Roberto","roberto@gmail.com","12999")
+EntradaSaida.mensagemCompleta(Datas.dataAtual())
 
-usuarioRoberto.adicionarAssinatura(100)
-usuarioRoberto.nome = "Roberto silva"
+// usuarioRoberto.adicionarAssinatura(100)
+// usuarioRoberto.nome = "Roberto silva"
 
-var email = EntradaSaida.entrada("Digite um email")
-var senha = EntradaSaida.entrada("Digite uma senha")
+// var email = EntradaSaida.entrada("Digite um email")
+// var senha = EntradaSaida.entrada("Digite uma senha")
 
-var resultado = usuarioJoao.fazerLogin(email,senha)
+// var resultado = usuarioJoao.fazerLogin(email,senha)
 
-EntradaSaida.mensagemCompleta(resultado)
+// EntradaSaida.mensagemCompleta(resultado)
 
-EntradaSaida.mensagemCompleta(usuarioJoao.validarAssinatura())
-usuarioJoao.assinatura = 100
-usuarioJoao.adicionarAssinatura(10)
-EntradaSaida.mensagemCompleta(usuarioJoao.validarAssinatura())
+// EntradaSaida.mensagemCompleta(usuarioJoao.validarAssinatura())
+// usuarioJoao.assinatura = 100
+// usuarioJoao.adicionarAssinatura(10)
+// EntradaSaida.mensagemCompleta(usuarioJoao.validarAssinatura())
 
-usuarioJoao.adicionarAssinatura(30)
-EntradaSaida.mensagemCompleta(usuarioJoao.validarAssinatura())
+// usuarioJoao.adicionarAssinatura(30)
+// EntradaSaida.mensagemCompleta(usuarioJoao.validarAssinatura())
